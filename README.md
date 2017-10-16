09dea82a24b18acbda4fb9cbe0f46cb451dc70ef
timothyfranzke <timothyfranzke@gmail.com>
2017-10-16 08:44:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# k-spaces-admin-portal
+Authentication front end for k-spaces
---
+++ src/app/index.module.js
@@ -0,0 +1,39 @@
+(function ()
+{
+    'use strict';
+
+    /**
+     * Main module of the Fuse
+     */
+    angular
+        .module('fuse', [
+
+            // Core
+            'app.core',
+
+            // Navigation
+            'app.navigation',
+
+            // Toolbar
+            'app.toolbar',
+
+            // Quick Panel
+            'app.quick-panel',
+
+            //Pages
+            'app.pages',
+
+            // Apps
+            'app.calendar',
+            'app.contacts',
+            'app.chat',
+            'app.e-commerce',
+            'app.mail',
+            'app.notes',
+            'app.configuration',
+            'app.manager',
+            //Services
+            'app.common',
+            'app.avatar-generator'
+        ]);
+})();
